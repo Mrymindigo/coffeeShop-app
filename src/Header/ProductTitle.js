42f606ec4ca665d8986269a9fcc451ce482e1c95
Mrymindigo <dmaryam784@gmail.com>
2025-06-01 16:04:12 +0330

    porduct title component is complate

--- src/Header/ProductTitle.js
+++ src/Header/ProductTitle.js
@@ -6,6 +6,11 @@ export default function ProductTitle() {
     return (
         <div className="productTitle">
             <h1>OUR Coffee OFFER</h1>
+            <div className="dotIcon">
+               <span></span>
+               <span></span>
+               <span></span>
+            </div>
         </div>
     )
 }
